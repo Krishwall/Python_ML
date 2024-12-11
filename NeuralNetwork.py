a65c4246fc7505e3a124312f4a64393eb671dae6
krishwall <krishjaiswal523@gmail.com>
2024-12-11 15:20:29 +0530

    # softmax, cross entropy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,8 +1,46 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
 
-inputs=[1.5,5.3,2.7]
-weights=[4.2,6.1,8.3]
+# inputs=[0,2,-1,3.3,-2.7,1.1,2.2,-100]
+# output=[]
 
-bias=3
+# for i in inputs :
+#     output.append(max(0,i))
 
-output=inputs[0]*weights[0]+inputs[1]*weights[1]+inputs[2]*weights[2]+bias
-print(output)
+# print(output)
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights=np.random.randn(n_inputs,n_neurons)
+        self.biases=np.zeros((1,n_neurons))
+        pass
+    def forward(self,inputs):
+        self.output=np.dot(inputs,self.weights)+self.biases
+
+
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output=np.maximum(0,inputs)
+
+class Activation_Softmax:
+    def forward(self,inputs):
+        exp_values= np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilities=exp_values/np.sum(exp_values,axis=1,keepdims=True)
+        self.output=probabilities
+X,y=spiral_data(samples=100, classes=3)
+
+dense1=Layer_Dense(2,3) # 2 inputs - x,y coordinates, 3 neurons
+activation1= Activation_ReLU()
+
+dense2=Layer_Dense(3,3) # 3 since output of previous layer is 3
+activation2 =Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
